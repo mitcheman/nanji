fd5c63d83ce33f74f7bfdf99b6d9026606981062
fegananca <fegananca@gmail.com>
2022-06-18 12:25:10 +0200

    add: set up ts config

--- src/components/header-comp.jsx
+++ src/components/header-comp.jsx
@@ -1,47 +1,85 @@
-
-import '../css/header.css'
-import React from "react";
-import {BrowserRouter as Router} from "react-router-dom";
+import '../css/header.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { MdOutlineAddPhotoAlternate, MdOutlineLogout } from 'react-icons/md';
-import { BsPersonLinesFill, BsPersonFill } from 'react-icons/bs'
+import { BsPersonLinesFill, BsPersonFill } from 'react-icons/bs';
 import { isMobile } from 'react-device-detect';
 
-export function Header({signOut, user, currentFriend}) {
+// interface Props {
+//   user: String;
+//   signOut: String;
+//   currentFriend: String;
+// }
 
-    const name = user.attributes.given_name + ' ' + user.attributes.family_name
+export function Header({ signOut, user, currentFriend }) {
+  const name = user.attributes.given_name + ' ' + user.attributes.family_name;
 
-    if(!isMobile) {
+  if (!isMobile) {
     return (
-        <div className='header'>
-                <a id="nanji" href='/'>Nanji</a>
+      <div className="header">
+        <a id="nanji" href="/">
+          Nanji
+        </a>
         <Router>
-        <div id="userinfo">
-            <a id="username" href='/'>{name}</a>
-            {(currentFriend) ? <div id="friendname"><h5>Current Profile&ensp;|</h5><p>&ensp;{currentFriend.given_name + ' ' + currentFriend.family_name}</p></div>: ''}
-        </div>
-        <nav>
+          <div id="userinfo">
+            <a id="username" href="/">
+              {name}
+            </a>
+            {currentFriend ? (
+              <div id="friendname">
+                <h5>Current Profile&ensp;|</h5>
+                <p>
+                  &ensp;
+                  {currentFriend.given_name + ' ' + currentFriend.family_name}
+                </p>
+              </div>
+            ) : (
+              ''
+            )}
+          </div>
+          <nav>
             <div id="nav-buttons">
-                <a href='/Friends'><BsPersonLinesFill /></a>
-                <a href='/newPost'><MdOutlineAddPhotoAlternate /></a>
-                <a href='/Account'><BsPersonFill /></a>
-                <button onClick={signOut}><MdOutlineLogout /></button>
+              <a href="/Friends">
+                <BsPersonLinesFill />
+              </a>
+              <a href="/newPost">
+                <MdOutlineAddPhotoAlternate />
+              </a>
+              <a href="/Account">
+                <BsPersonFill />
+              </a>
+              <button onClick={signOut}>
+                <MdOutlineLogout />
+              </button>
             </div>
-        </nav>
+          </nav>
         </Router>
-        </div>
-    )
-} else {
+      </div>
+    );
+  } else {
     return (
-        <div className='header'>
-                <a id="nanji" href='/'>Nanji</a>
+      <div className="header">
+        <a id="nanji" href="/">
+          Nanji
+        </a>
         <Router>
-        <div id="userinfo">
-            {(currentFriend) ? <div id="friendname"><h5>Current Profile&ensp;|</h5><p>&ensp;{currentFriend.given_name + ' ' + currentFriend.family_name}</p></div>:
-            <a id="username" href='/'>{name}</a>}
-        </div>
+          <div id="userinfo">
+            {currentFriend ? (
+              <div id="friendname">
+                <h5>Current Profile&ensp;|</h5>
+                <p>
+                  &ensp;
+                  {currentFriend.given_name + ' ' + currentFriend.family_name}
+                </p>
+              </div>
+            ) : (
+              <a id="username" href="/">
+                {name}
+              </a>
+            )}
+          </div>
         </Router>
-        </div>
-    )
+      </div>
+    );
+  }
 }
-}
-
