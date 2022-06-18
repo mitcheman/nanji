e9580be1b0dea665542af0fe8189f4be8a9207bc
XavierFabregat <xavi.fabregat.pous@gmail.com>
2022-06-18 20:27:08 +0200

    added types to friends-page

--- src/pages/friends-page.tsx
+++ src/pages/friends-page.tsx
@@ -1,36 +1,44 @@
-import { Search } from "../components/search-comp";
-import { FriendsList } from "../components/friendsList-comp";
-import { RequestList } from "../components/requestList-comp";
-import "../css/friends.css";
-import { useState } from "react";
+import { Search } from '../components/search-comp';
+import { FriendsList } from '../components/friendsList-comp';
+import { RequestList } from '../components/requestList-comp';
+import '../css/friends.css';
+import React, { useState } from 'react';
+import { CognitoUserType, DynamoUserType, FriendType } from '../Shared/Types';
 
-export function Friends({ user, friends, setFriends }) {
-	const [outGoingRequestsUsers, setOutGoingRequestsUsers] = useState([]);
-	const [incomingRequestsUsers, setIncomingRequestsUsers] = useState([]);
+export const Friends: React.FC<{
+  user: CognitoUserType;
+  friends: FriendType[];
+  setFriends: React.Dispatch<React.SetStateAction<FriendType[]>>;
+}> = ({ user, friends, setFriends }) => {
+  const [outGoingRequestsUsers, setOutGoingRequestsUsers] = useState<
+    DynamoUserType[]
+  >([]);
+  const [incomingRequestsUsers, setIncomingRequestsUsers] = useState<
+    DynamoUserType[]
+  >([]);
 
-	return (
-		<>
-			<div id="friends">
-				<div id="addfriends">
-					<Search
-						user={user}
-						outGoingRequestsUsers={outGoingRequestsUsers}
-						setOutGoingRequestsUsers={setOutGoingRequestsUsers}
-					/>
-					<RequestList
-						user={user}
-						outGoingRequestsUsers={outGoingRequestsUsers}
-						setOutGoingRequestsUsers={setOutGoingRequestsUsers}
-						incomingRequestsUsers={incomingRequestsUsers}
-						setIncomingRequestsUsers={setIncomingRequestsUsers}
-						friends={friends}
-						setFriends={setFriends}
-					/>
-				</div>
-				<div id="friendspagelist">
-					<FriendsList user={user} friends={friends} setFriends={setFriends} />
-				</div>
-			</div>
-		</>
-	);
-}
+  return (
+    <>
+      <div id="friends">
+        <div id="addfriends">
+          <Search
+            user={user}
+            setOutGoingRequestsUsers={setOutGoingRequestsUsers}
+          />
+          <RequestList
+            user={user}
+            outGoingRequestsUsers={outGoingRequestsUsers}
+            setOutGoingRequestsUsers={setOutGoingRequestsUsers}
+            incomingRequestsUsers={incomingRequestsUsers}
+            setIncomingRequestsUsers={setIncomingRequestsUsers}
+            friends={friends}
+            setFriends={setFriends}
+          />
+        </div>
+        <div id="friendspagelist">
+          <FriendsList user={user} friends={friends} setFriends={setFriends} />
+        </div>
+      </div>
+    </>
+  );
+};
