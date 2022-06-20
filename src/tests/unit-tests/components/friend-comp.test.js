7ccc1f6ab202f79e3391042e5b4e116a65784901
XavierFabregat <xavi.fabregat.pous@gmail.com>
2022-06-20 12:49:07 +0200

    Added test for friend-comp to test navigation, still not working tho!!

--- src/tests/unit-tests/components/friend-comp.test.js
+++ src/tests/unit-tests/components/friend-comp.test.js
@@ -1,7 +1,10 @@
 import { Friend } from '../../../components/friend-comp';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
 
-it("should render a friend's info", async () => {
+it("should render a friend's info", () => {
   const friendInfo = {
     id: 'eifefiief-ii32839240-2nefn2n4in29f',
     given_name: 'Jhon',
@@ -16,4 +19,36 @@ it("should render a friend's info", async () => {
 
   expect(fullName).toHaveTextContent('Name | Jhon Doe');
   expect(preferredUsername).toHaveTextContent('Username | Jhonny');
+});
+
+// Test not working, have to revisit!!
+it('should redirect to friends dshboard when you click on it', async () => {
+  const friendInfo = {
+    id: 'eifefiief-ii32839240-2nefn2n4in29f',
+    given_name: 'Jhon',
+    family_name: 'Doe',
+    preferred_username: 'Jhonny',
+  };
+
+  const history = createMemoryHistory();
+
+  render(
+    <MemoryRouter histroy={history}>
+      <Friend friend={friendInfo} />
+    </MemoryRouter>,
+  );
+
+  const friendsLink = screen.getByTestId('friendalink');
+
+  console.log(friendsLink, 'friendsLink');
+
+  await userEvent.click(friendsLink);
+
+  console.log(history.location.pathname);
+
+  expect(history.location.pathname).toBe(
+    '/user/eifefiief-ii32839240-2nefn2n4in29f',
+  );
+
+  // expect(screen.getByText('Current Profile')).toBeInTheDocument();
 });
