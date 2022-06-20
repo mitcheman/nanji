707e0b5990da6f15d55f9b59245a58f8ac3596f5
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-20 18:06:10 +0200

    checkpoint before pull request

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -2,6 +2,5 @@ import { PostType } from '../types/PostType';
 const moment = require('moment');
 
 export const sortData = (arr: PostType[]) => {
-  console.log(arr, 'sorting arraayyyy');
   arr.sort((a, b) => moment(b.date).unix() - moment(a.date).unix());
 }
