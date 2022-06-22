834fc14e324b410ca9bbabb4d6ade04d7c3b10aa
noelcv <79803356+noelcv@users.noreply.github.com>
2022-06-22 12:21:13 +0200

    test: create test suite for Create Account Action

--- cypress/e2e/nanji/spec.cy.ts
+++ cypress/e2e/nanji/spec.cy.ts
@@ -1,6 +1,64 @@
+/// <reference types="Cypress" />
+
 describe('Launching Nanji', () => {
-  it('Visits Nanji Sign up Page', () => {
+  it('should visit Nanji', () => {
     cy.visit('http://localhost:3000')
+  })
+  
+})
+
+
+describe('Creating an account on Nanji', () => {
+  it('should visit Nanji Sign up Page', () => {
+    cy.visit('http://localhost:3000')
+  })
+  
+  it('finds the content "Create Account"', () => {
+    cy.contains('Create Account')
+  })
+  
+  it('clicks the content "Create Account"', () => {
+    cy.contains('Create Account').click();
+  })
+  
+  it('should be able to find email input field and type in it', () => {
+    cy.get('[placeholder="Email"]').type('karl.marx@gmail.com');
+  })
+  
+  it('should be able to find password input field and type in it', () => {
+    cy.get('[placeholder="Password"]').type('DasK4pit4L$$$');
+  })
+  
+  it('should notify the user when password is wrong', () => {
+    cy.get('[placeholder="Confirm Password"]').type('DasK4pit4L');
+  })
+  
+  it('should make the password input visible when toggling the visibility button', () => {
+    cy.get('[class="amplify-button amplify-field-group__control amplify-field__show-password"]').click({multiple: true});
+  })
+  
+  it('should be able to correct the password', () => {
+    cy.get('[placeholder="Confirm Password"]').type('$$$');
+  })
+  
+  it('should make the password input hidden when toggling the visibility button', () => {
+    cy.get('[class="amplify-button amplify-field-group__control amplify-field__show-password"]').click({multiple: true});
+  })
+  
+  it('should be able to find the Given Name input field and type in it', () => {
+    cy.get('[placeholder="Given Name"]').type('Karl');
+  })
+  
+  it('should be able to find the Given Name input field and type in it', () => {
+    cy.get('[placeholder="Family Name"]').type('Marx');
+  })
+  
+  it('should be able to find the Preferred Username input field and type in it', () => {
+    cy.get('[placeholder="Preferred Username"]').type('km');
+  })
+  
+  it('should be able to submit form and create Account', () => {
+    cy.get('[type="submit"]').click();
   })
   
 })
