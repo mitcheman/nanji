f6b860e81cf3e63e64cf5dc07f984a8887bbba75
Mitchell <mitchellryansmith@gmail.com>
2022-07-13 16:11:20 +0300

    0.7.3
    updated signin screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator, Image, View, Text } from '@aws-amplify/ui-react';
 import './index.css';
 import '@aws-amplify/ui-react/styles.css';
 import { Header } from './components/header-comp';
@@ -15,49 +15,92 @@ function App({ signOut, user }) {
   const [friends, setFriends] = useState([]);
   const [currentFriend, setCurrentFriend] = useState();
 
+  const components = {
+    Header() {
+      return (
+        <View textAlign='center' margin={30}>
+          <Image alt='Nanji logo' src='/logo_transparent.png' />
+        </View>
+      );
+    },
+
+    Footer() {
+      return (
+        <View textAlign='center' margin={10}>
+          <a
+            href='https://youtu.be/VP4_ufbt_Dg'
+            style={{
+              textDecoration: 'none',
+              color: 'grey',
+              padding: 10,
+              borderRadius: 5,
+              margin: 5,
+            }}
+          >
+            Checkout the Beta video here
+          </a>
+        </View>
+      );
+    },
+  };
+
   return (
     <>
-      <Header signOut={signOut} user={user} currentFriend={currentFriend} />
-      <Router>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <Dashboard
-                user={user}
-                signout={signOut}
-                friends={friends}
-                setFriends={setFriends}
-              />
-            }
-          />
-          <Route
-            path='/Friends'
-            element={
-              <Friends user={user} friends={friends} setFriends={setFriends} />
-            }
-          />
-          <Route path='/NewPost' element={<NewPost user={user} />} />
-          <Route
-            path='/user/:id'
-            element={
-              <UserFriend
-                user={user}
-                friends={friends}
-                setFriends={setFriends}
-                currentFriend={currentFriend}
-                setCurrentFriend={setCurrentFriend}
-              />
-            }
-          />
-          <Route
-            path='/Account'
-            element={<Account user={user} signOut={signOut} />}
-          />
-        </Routes>
-      </Router>
+      <Authenticator components={components}>
+        {({ signOut, user }) => (
+          <>
+            <Header
+              signOut={signOut}
+              user={user}
+              currentFriend={currentFriend}
+            />
+            <Router>
+              <Routes>
+                <Route
+                  path='/'
+                  element={
+                    <Dashboard
+                      user={user}
+                      signout={signOut}
+                      friends={friends}
+                      setFriends={setFriends}
+                    />
+                  }
+                />
+                <Route
+                  path='/Friends'
+                  element={
+                    <Friends
+                      user={user}
+                      friends={friends}
+                      setFriends={setFriends}
+                    />
+                  }
+                />
+                <Route path='/NewPost' element={<NewPost user={user} />} />
+                <Route
+                  path='/user/:id'
+                  element={
+                    <UserFriend
+                      user={user}
+                      friends={friends}
+                      setFriends={setFriends}
+                      currentFriend={currentFriend}
+                      setCurrentFriend={setCurrentFriend}
+                    />
+                  }
+                />
+                <Route
+                  path='/Account'
+                  element={<Account user={user} signOut={signOut} />}
+                />
+              </Routes>
+            </Router>
+          </>
+        )}
+      </Authenticator>
     </>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
